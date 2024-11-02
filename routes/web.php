6dfe25c55732187320eb2560622b81a2ae5e9ee4
Dmitry <funbrainfun@gmail.com>
2024-11-02 14:44:58 +0200

    clean

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+
+Route::group(['namespace' => 'admin', 'prefix' => 'admin'], function(){
+    Route::get('/main', [MainController::class, 'index']);
+});
+
+
+
+Auth::routes();
+
+
